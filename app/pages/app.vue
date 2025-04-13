b16e1243f6543429dcc05140299731eae602f800
unmilley <1337@milley.uno>
2025-04-14 01:39:14 +0500

    feat: `/index -> web & /app -> app `

---
+++ app/pages/app.vue
@@ -0,0 +1,7 @@
+<template>
+	<div>
+		<h1 class="uppercase">THIS IS APP</h1>
+	</div>
+</template>
+
+<script lang="ts" setup></script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,30 @@
 <template>
-	<div>meow</div>
+	<div class="hero bg-base-100 min-h-screen">
+		<div class="hero-content">
+			<div class="max-w-md">
+				<h1 class="text-5xl font-bold">Textypie</h1>
+				<p class="py-6">
+					A scripting notebook for developers! Modify, adapt, convert in safety – now you can do it all!
+				</p>
+				<div class="flex gap-4">
+					<nuxt-link to="/app" class="btn h-16 alert gap-2 ~min-w-max">
+						<Icon name="game-icons:pie-slice" size="2.2rem" />
+						<div>
+							<h3 class="text-xs">Web version</h3>
+							<div class="text-lg font-bold">To app</div>
+						</div>
+					</nuxt-link>
+					<tauri-link href="https://github.com/unmilley/textypie" target="_blank" class="btn h-16 alert w-max">
+						<Icon name="bx:bxl-github" size="2.2rem" />
+						<div>
+							<h3 class="text-xs">Get in on</h3>
+							<div class="text-lg font-bold">Github</div>
+						</div>
+					</tauri-link>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script lang="ts" setup></script>
