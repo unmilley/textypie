7580f195053a06cbcb6abf984e509fa65b485e74
unmilley <1337@milley.uno>
2025-04-13 22:16:50 +0500

    feat: `init tauri`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+	compatibilityDate: '2025-03-22',
+	devtools: { enabled: true },
+	ssr: false,
+
+	vite: {
+		clearScreen: false,
+		envPrefix: ['VITE_', 'TAURI_'],
+		server: {
+			strictPort: true,
+		},
+	},
 })
