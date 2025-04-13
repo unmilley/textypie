6355f0ae3c32dc3544c7ecbc8883718c0c997683
unmilley <1337@milley.uno>
2025-04-13 22:25:54 +0500

    feat: `migrate to nuxt/4`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,13 @@
 export default defineNuxtConfig({
 	compatibilityDate: '2025-03-22',
 	devtools: { enabled: true },
+	future: { compatibilityVersion: 4 },
 	ssr: false,
 
+	imports: { dirs: ['./types'] },
 	vite: {
 		clearScreen: false,
 		envPrefix: ['VITE_', 'TAURI_'],
-		server: {
-			strictPort: true,
-		},
+		server: { strictPort: true },
 	},
 })
