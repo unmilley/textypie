43814bd05afe43a9bdd8164f1fe2ebbde266d848
unmilley <1337@milley.uno>
2025-04-14 16:55:00 +0500

    feat: `added btn for open actionbar`

---
+++ app/components/action-bar/button.vue
@@ -0,0 +1,18 @@
+<template>
+	<button class="btn btn-sm border border-base-content/50" @click.prevent.stop="open()">
+		<Icon name="bx:search" size="1.25rem" class="opacity-50" />
+		<div class="divider divider-horizontal !w-1 mx-0"></div>
+		<span class="w-32 text-left text-sm opacity-80">{{ isVisible ? 'Select an action' : 'Search' }}</span>
+		<kbd class="kbd kbd-sm min-w-14 capitalize">{{ combination.map(({ key }) => key).join(' ') }}</kbd>
+	</button>
+</template>
+
+<script lang="ts" setup>
+const { isVisible, open } = useModal('action-bar')
+
+/* TODO: change useState to any storage in browser */
+const combination = useState<Combinations>('master-key', () => [
+	{ key: 'Ctrl', code: 'ControlLeft' },
+	{ key: 'q', code: 'KeyQ' },
+])
+</script>
--- app/components/tauri/titlebar.vue
+++ app/components/tauri/titlebar.vue
@@ -7,13 +7,15 @@
 			v-if="appWindow"
 		>
 			<div class="navbar-start"></div>
-			<div class="navbar-center"></div>
+			<div class="navbar-center">
+				<ActionBarButton />
+			</div>
 			<div class="navbar-end">
-				<button class="btn btn-ghost btn-square rounded-none" @click.stop="appWindow.minimize()">
+				<button class="btn btn-ghost btn-square rounded-none" @click.prevent.stop="appWindow.minimize()">
 					<Icon name="bx:minus" size="1.2rem" />
 				</button>
 				<button class="btn btn-ghost btn-square rounded-none" @click.stop="appWindow.toggleMaximize()">
-					<Icon :name="iconMaximized.name" :size="iconMaximized.size" class="pointer-events-none" />
+					<Icon :name="iconMaximized.name" :size="iconMaximized.size" />
 				</button>
 				<button class="btn btn-ghost btn-square hover:btn-error rounded-none" @click.prevent.stop="appWindow.close()">
 					<Icon name="bx:x" size="1.25rem" />
@@ -27,19 +29,23 @@
 import { getCurrentWindow, type Window } from '@tauri-apps/api/window'
 
 const appWindow = ref<Window>(getCurrentWindow())
+
 const [isMaximized, toggleLocalMaximized] = useToggle(false)
 const iconMaximized = computed(() => {
 	return isMaximized.value ? { name: 'bx:windows', size: '0.95rem' } : { name: 'bx:window', size: '0.85rem' }
 })
 
-appWindow.value.listen('tauri://resize', async () => {
+const checkMaximized = useThrottleFn(async () => {
 	toggleLocalMaximized(await appWindow.value?.isMaximized())
-})
+}, 1000)
+
+appWindow.value.listen('tauri://resize', checkMaximized)
 
 const titlebar = useTemplateRef('titlebar')
 useEventListener(titlebar, 'mousedown', (e) => {
 	if (e.buttons !== 1) return
-	if (['BUTTON', 'svg'].includes((e.target as HTMLElement).nodeName)) return
+
+	if (['svg', 'BUTTON', 'SPAN', 'KBD'].includes((e.target as HTMLElement).nodeName)) return
 	e.preventDefault()
 	e.detail === 2 ? appWindow.value?.toggleMaximize() : appWindow.value?.startDragging()
 })
