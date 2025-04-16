7207e97fc489beed9e25ea0a1370aa44a35c2165
unmilley <1337@milley.uno>
2025-04-16 15:54:07 +0500

    feat: `update index page`

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,13 +2,16 @@
 	<div class="hero bg-base-100 min-h-screen">
 		<div class="hero-content">
 			<div class="max-w-md">
-				<h1 class="text-5xl font-bold">Textypie</h1>
+				<h1 class="text-5xl font-bold flex items-center gap-2">
+					<Icon name="textypie:logo-fill" size="3rem" />
+					<span>Textypie</span>
+				</h1>
 				<p class="py-6">
 					A scripting notebook for developers! Modify, adapt, convert in safety – now you can do it all!
 				</p>
 				<div class="flex gap-4">
 					<nuxt-link to="/app" class="btn h-16 alert gap-2 ~min-w-max">
-						<Icon name="game-icons:pie-slice" size="2.2rem" />
+						<Icon name="textypie:logo" size="2.2rem" />
 						<div>
 							<h3 class="text-xs">Web version</h3>
 							<div class="text-lg font-bold">To app</div>
